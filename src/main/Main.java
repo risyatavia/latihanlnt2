85544a7533efa74155c3023b3ec40eaab8dd748c
risyatavia <verisya.autavia@binus.ac.id>
2023-01-06 15:57:25 +0700

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,99 @@
+//package main;
+//
+//public class Main {
+//	//entry point codingan
+//	public static void main(String[] args) {
+//	
+//		//arithmetic operators + - * / %
+//		//+ tambah, - kurang, * kali, / bagi, % modulo
+//	
+//		//syso + ctrl + spasi
+//		
+//		//aritmethic operators
+//		System.out.println(5 + 6 * 2); //bisa ditambah lg juga contohnya (5 + 6 + 7 + 8) bisa kali bagi dll juga
+//		System.out.println(8 - 3);
+//		System.out.println(4 * 2);
+//		System.out.println(8 / 2);
+//		//modulo adalah sisa pembagian
+//		System.out.println(10 % 3);
+//		//jadi 10 dibagi 3 sisa 1 jadi print 1
+//		//run -> F11
+//		
+//		//assignment operators
+//		System.out.println("assignment op");
+//		//data type dll
+//		
+//		int angka = 10;
+//		float iniFloat = 20f;
+//		System.out.println(angka);
+//		//= buat masukin angka ke dalam nama
+//		//shorthand operators
+//		angka += 5; //angka = angka + 5 (jadi nambah angka ke yg tadi)
+//		System.out.println(angka);
+//		angka -= 2;
+//		System.out.println(angka);
+//		angka *= 4;
+//		System.out.println(angka);
+//		angka /= 2;
+//		System.out.println(angka);
+//		angka %= 2;
+//		System.out.println(angka); 
+//		
+//		//increment dan decrement
+//		angka++; //angka = angka + 1 //kepake di for loop
+//		System.out.println(angka);
+//		angka--; //angka = angka - 1
+//		System.out.println(angka);
+//		
+////		System.out.println(++angka); //tambahin dulu baru print
+////		System.out.println(angka);
+//		System.out.println(angka++); //print dulu baru ditambah
+//		System.out.println(angka); //baru keluar angkanya dari angka++
+//		
+//		//relational operators
+//		System.out.println("relational operators");
+//		
+//		//== artinya equal/seimbang/sama
+//		System.out.println(1==1); //print true karena seimbang kalo ga seimbang print false
+//		
+//		//!= artinya not equal/berbeda
+//		System.out.println(1 !=2); //print true karena ga seimbang kalo seimbang print false
+//		
+//		//< artinya lebih kecil/lesser than
+//		System.out.println(2 < 5); //print true karena lebih kecil kalo lebih besar print false
+//		
+//		//> artinya lebih besar/greater than
+//		System.out.println(10 > 4); //print true karena lebih beasr kalo lebih kecil print false
+//
+//		//<= artinya lebih kecil atau sama dengan/ lesser than or equal
+//		System.out.println(5 <= 5); ////print true karena angka pertama lebih kecil kalo angka pertama lebih besar print false
+//	
+//		//logical operators
+//		//&& || !
+//		//&& -> AND
+//		System.out.println((1 > 5) && (4 > 2)); //kalo kiri dan kanan benar maka akan print true kalo salah satu yg benar maka tetap di prnit false
+//		//					false		true 	= maka false
+//		
+//		// || -> OR
+//		System.out.println((1 > 5) || (4 > 2)); // || kalo ini kalo salah satu salah maka di print true
+//		//					false		true	= maka true
+//		
+//		// ! -> kalau false jadi true
+//		//kalau true jadi false
+//		System.out.println(!(1 > 5)); //harusnya false tapi karena make ! jadi true pas di print
+//		
+//		//kalo precedence and associative operators itu + - * / tapi diutamakan yg terkuat dulu jadi kalo misalkan ada - sama * bakal lebih diutamain yg *
+//		
+//		//bitwise operators & | ^ ~
+//		System.out.println("bitwise operators"); //jarang dipake (bitwise tuh mainnya di binary nya angka)
+//		
+//		System.out.println(5 & 7); //bitwise AND (&)
+//		System.out.println(5 | 7); //bitwise OR (|)
+//		System.out.println(5 ^ 7); //bitwise XOR (^) mirip OR 
+//		System.out.println(~5); //bitwise NOT (~) semua yg 1 jadi 0, semua yg 0 jadi 1
+//		
+//		
+//		
+//	}
+//
+//}
